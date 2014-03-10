4e05348335f5d3ea0944c92c1d95ac183922816a
Willy Douglas <willygdouglas@gmail.com>
2014-03-09 22:31:16 -0600

    Converted all indentation to 4 spaces.

--- app.js
+++ app.js
@@ -17,63 +17,63 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.get('/', routes.index);
 
 app.get('/notebooks', function (req, res) {
-	model.listNotebooks(function (notebooks) {
-		res.send(200, notebooks);
-	});
+    model.listNotebooks(function (notebooks) {
+        res.send(200, notebooks);
+    });
 });
 
 app.get('/notebook/:guid', function (req, res) {
-	var guid = req.params.guid;
-	model.getNotebook(guid, function (notebook) {
-		res.send(200, notebook);
-	});
+    var guid = req.params.guid;
+    model.getNotebook(guid, function (notebook) {
+        res.send(200, notebook);
+    });
 });
 
 app.get('/notes', function (req, res) {
-	model.findNotesMetadata(function (noteData) {
-		//console.log('noteData',noteData);
-		noteData.notes.forEach(function (note) { note.url = '/notes/' + note.guid;});
-		res.send(200, noteData);
-	});
+    model.findNotesMetadata(function (noteData) {
+        //console.log('noteData',noteData);
+        noteData.notes.forEach(function (note) { note.url = '/notes/' + note.guid;});
+        res.send(200, noteData);
+    });
 });
 
 app.get('/notes/:noteId', function (req, res) {
-	var noteId = req.params.noteId;
-	var options = { 'content': true };
-	model.getNote(noteId, options, function (note) {
-		res.send(200, note);
-	});
+    var noteId = req.params.noteId;
+    var options = { 'content': true };
+    model.getNote(noteId, options, function (note) {
+        res.send(200, note);
+    });
 });
 
 app.post('/createNote', function (req, res) {
-	var title = req.body.title,
-		body = req.body.content || '';
-	model.createNote(title, body, {}, function( note ) {
-	  res.send(200, note);
-	});
+    var title = req.body.title,
+        body = req.body.content || '';
+    model.createNote(title, body, {}, function( note ) {
+      res.send(200, note);
+    });
 });
 /* $.get('/notes/' + id)
 $.get(url)
 */
 app.put('/updateNote', function (req, res) {
-	var guid = req.body.guid,
-		title = req.body.title || '',
-		body = req.body.content || '';
-	console.log('Going to update note...');
-	console.log('title: ' + title);
-	console.log('guid: ' + guid);
-	model.updateNote(guid, title, body, function() {
-		res.send(200);
-	});
+    var guid = req.body.guid,
+        title = req.body.title || '',
+        body = req.body.content || '';
+    console.log('Going to update note...');
+    console.log('title: ' + title);
+    console.log('guid: ' + guid);
+    model.updateNote(guid, title, body, function() {
+        res.send(200);
+    });
 });
 
 app.del('/notes/:guid', function (req, res) {
-	model.trashNote(req.params.guid, function (err, note) {
-		if (err) {
-			res.send(500, err);
-		}
-		res.send(200);
-	});
+    model.trashNote(req.params.guid, function (err, note) {
+        if (err) {
+            res.send(500, err);
+        }
+        res.send(200);
+    });
 });
 
 
--- public/javascript/front.js
+++ public/javascript/front.js
@@ -5,117 +5,117 @@
 
 // TODO put methods in Node.prototype
 var Note = function (title, content, guid) {
-	var note = $('<div class="note">' +
+    var note = $('<div class="note">' +
         '<input type="text" class="title" placeholder="Title"></input>' +
         '<div contenteditable class="content" data-placeholder="note"></div>' +
         '<div class="options">' +
-          '<input type="button" class="trash" value="Trash"></input>' +
+            '<input type="button" class="trash" value="Trash"></input>' +
         '</div>' +
       '</div>');
-	if (title !== undefined && title !== 'Untitled') {
-		note.find('.title').val(title);
-	}
-	else {
-		note.find('.title').hide();
-	}
-	if (content !== undefined) {
-		note.find('.content').append(content)
-		                     .on('click', 'a', function () {
-			// TODO: Handle link clicks
-			console.log( 'Clicked link: ' + $(this).attr('href') );
-		});
-	}
-	if (guid !== undefined) {
-		note.attr('data-guid', guid);
-	}
-	else {
-		note.addClass('new-note');
-	}
-	note.on('input', '.title, .content', function () {
-		note.addClass('changed');
-	});
-	note.on('focus', '.title, .content', function () {
-		note.addClass('active');
-		note.find('.title').show();
-	});
-	note.on('blur',  '.title, .content', function () {
-		note.removeClass('active');
-		var $title = note.find('.title');
-		if ( $title.val() === '') {
-			$title.hide();
-		}
-	});
-	note.on('click', '.trash', function () {
-		if (guid !== undefined) {
-			$.ajax('/notes/' + guid, {
-				type: 'DELETE'
-			});
-		}
-		// TODO: Check if this will cause memory leaks 
-		// from old references
-		note.remove();
-	});
+    if (title !== undefined && title !== 'Untitled') {
+        note.find('.title').val(title);
+    }
+    else {
+        note.find('.title').hide();
+    }
+    if (content !== undefined) {
+        note.find('.content').append(content)
+                             .on('click', 'a', function () {
+            // TODO: Handle link clicks
+            console.log( 'Clicked link: ' + $(this).attr('href') );
+        });
+    }
+    if (guid !== undefined) {
+        note.attr('data-guid', guid);
+    }
+    else {
+        note.addClass('new-note');
+    }
+    note.on('input', '.title, .content', function () {
+        note.addClass('changed');
+    });
+    note.on('focus', '.title, .content', function () {
+        note.addClass('active');
+        note.find('.title').show();
+    });
+    note.on('blur',  '.title, .content', function () {
+        note.removeClass('active');
+        var $title = note.find('.title');
+        if ( $title.val() === '') {
+            $title.hide();
+        }
+    });
+    note.on('click', '.trash', function () {
+        if (guid !== undefined) {
+            $.ajax('/notes/' + guid, {
+                type: 'DELETE'
+            });
+        }
+        // TODO: Check if this will cause memory leaks 
+        // from old references
+        note.remove();
+    });
 
-	return note;
+    return note;
 };
 
 $( document ).ready( function () {
-	// Older versions of Android don't support contentEditable
-	// will need some sort of fallback in future (Possibly to textarea).
-	// This check's if it is supported
-	var isContentEditable = 'isContentEditable' in document.createElement('span');
+    // Older versions of Android don't support contentEditable
+    // will need some sort of fallback in future (Possibly to textarea).
+    // This check's if it is supported
+    var isContentEditable = 'isContentEditable' in document.createElement('span');
 
-	$('#notes').prepend( new Note() );
+    $('#notes').prepend( new Note() );
 
-	$('#new-note-button').on('click', function() {
-		$('#notes').prepend( new Note() );
-	});
+    $('#new-note-button').on('click', function() {
+        $('#notes').prepend( new Note() );
+    });
 
-	// load some recent notes
-	$.get('/notes', function ( data ) {
-		for (var i = 0; i < data.notes.length; ++i) {
-			$('#notes').append( new Note(data.notes[i].title, null, data.notes[i].guid) );
-		}
-	}).then( function () {
-		$('.note:not(.new-note)').each( function (index, element) {
-			$.get('notes/' + $( element ).data('guid'), function ( note ) {
-				$( element ).find('.content').append( note.content );
-			});
-		});
-	});
+    // load some recent notes
+    $.get('/notes', function ( data ) {
+        for (var i = 0; i < data.notes.length; ++i) {
+            $('#notes').append( new Note(data.notes[i].title, null, data.notes[i].guid) );
+        }
+    }).then( function () {
+        $('.note:not(.new-note)').each( function (index, element) {
+            $.get('notes/' + $( element ).data('guid'), function ( note ) {
+                $( element ).find('.content').append( note.content );
+            });
+        });
+    });
 
-	// Sync
-	$('#sync-button').on('click', function () {
-		$('.note.new-note').each( function (index, element) {
-			var noteContent = {
-				'title': $( element ).find('.title').val(),
-				'content': $( element ).find('.content').html()
-			};
-			if (noteContent.title !== '' || noteContent.content !== '') {
-				$.post('/createNote', noteContent, function ( response ) {
-					$( element ).attr('data-guid', response.guid);
-				});
-				$( element ).removeClass('new-note').removeClass('changed');
-				// Wrap content in <en-note></en-note> xml for Evernote
-				var content = $( element ).find('.content');
-				content.html('<en-note>' + content.html() + '</en-note>');
-			}
-		});
+    // Sync
+    $('#sync-button').on('click', function () {
+        $('.note.new-note').each( function (index, element) {
+            var noteContent = {
+                'title': $( element ).find('.title').val(),
+                'content': $( element ).find('.content').html()
+            };
+            if (noteContent.title !== '' || noteContent.content !== '') {
+                $.post('/createNote', noteContent, function ( response ) {
+                    $( element ).attr('data-guid', response.guid);
+                });
+                $( element ).removeClass('new-note').removeClass('changed');
+                // Wrap content in <en-note></en-note> xml for Evernote
+                var content = $( element ).find('.content');
+                content.html('<en-note>' + content.html() + '</en-note>');
+            }
+        });
 
-		$('.note.changed').each( function (index, element) {
-			$.ajax('/updateNote', {
-				type: 'PUT',
-				dataType: 'json',
-				data: {
-					'guid': $(this).data('guid'),
-					'title': $(this).find('.title').val(),
-					'content': $(this).find('.content').html()
-				},
-				success: function ( response ) {
-					console.log('' + response + ' updated note success');
-				}
-			});
-			$(this).removeClass('changed');
-		});
-	});
+        $('.note.changed').each( function (index, element) {
+            $.ajax('/updateNote', {
+                type: 'PUT',
+                dataType: 'json',
+                data: {
+                    'guid': $(this).data('guid'),
+                    'title': $(this).find('.title').val(),
+                    'content': $(this).find('.content').html()
+                },
+                success: function ( response ) {
+                    console.log('' + response + ' updated note success');
+                }
+            });
+            $(this).removeClass('changed');
+        });
+    });
 });
